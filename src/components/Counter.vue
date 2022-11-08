34f2be58162adb452f33c4afa0eeeab51d0d3ba0
Jandronit <jandrohen@gmail.com>
2022-11-08 19:23:21 +0100

    (lesson 144 course vue) Mutations

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -3,6 +3,10 @@
   <h2> Direct access: {{ $store.state.count }}</h2>
   <h2> Computed: {{ countComputed }}</h2>
   <hr>
+  <button @click="increment">+1</button>
+  <button @click="incrementBy5">+5</button>
+  <button>Random</button>
+  <hr>
   <h1>mapState</h1>
   <h2> mapState: {{ count }}</h2>
   <h2> lastMutation: {{ lastMutation }}</h2>
@@ -24,6 +28,15 @@ export default {
   //   count: state => state.count,
   //   lastMutation: state => state.lastMutation,
   // }),
+  },
+
+  methods: {
+    increment() {
+      this.$store.commit('increment');
+    },
+    incrementBy5() {
+      this.$store.commit('incrementBy', 5);
+    },
   }
 
 }
